4efccb03f8c4d03efd8055c6e328f2a85389d54c
SergeGaliullin <sergei.galiullin@gmail.com>
2017-04-04 15:22:49 -0300

    Add Employee model

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    department = models.CharField(max_length=50)
+
