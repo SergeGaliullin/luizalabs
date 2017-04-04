fb28c19c9473609ea92b2f86b58157f4ccab90c8
SergeGaliullin <sergei.galiullin@gmail.com>
2017-04-04 15:39:04 -0300

    Add methods to get post delete

--- employee/views.py
+++ employee/views.py
@@ -3,7 +3,17 @@ from rest_framework import generics
 from .models import Employee
 
 
-class EmployeeList(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class EmployeeList(mixins.CreateModelMixin, mixins.ListModelMixin,
+                     mixins.DestroyModelMixin, generics.GenericAPIView):
     queryset = Employee.object.all()
     serializer_class = EmployeeSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
     
